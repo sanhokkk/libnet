af6553463e00970f6bc2857f2285de522d1116dd
Sanu7D0 <ddalkisoup21@gmail.com>
2024-01-24 00:05:00 -0800

    Add Packet::WriteHeader function

--- skymarlin/network/packet/Packet.hpp
+++ skymarlin/network/packet/Packet.hpp
@@ -8,10 +8,11 @@ class Session;
 }
 
 namespace skymarlin::network::packet {
-using PacketLength = u16;
-using PacketType = u16;
+using PacketLength = u16; // Exclude header length
+using PacketType = u8;
+// Reserve u8 for crypto type
 
-constexpr static size_t PACKET_HEADER_SIZE = sizeof(u32);
+constexpr static size_t PACKET_HEADER_SIZE = sizeof(PacketLength) + sizeof(PacketType);
 
 
 class Packet : boost::noncopyable {
@@ -22,8 +23,16 @@ public:
 
     virtual void Serialize(boost::asio::mutable_buffer& buffer) const = 0;
 
-    virtual void Deserialize(boost::asio::mutable_buffer& buffer)= 0;
+    virtual void Deserialize(const boost::asio::mutable_buffer& buffer) = 0;
 
     virtual void Handle(std::shared_ptr<Session> session) = 0;
+
+    virtual size_t Length() const = 0;
+
+    static void WriteHeader(byte* buffer, const PacketLength length, const PacketType type) {
+        //TODO: Check buffer has at least header size
+        utility::MutableByteBuffer writer(buffer, PACKET_HEADER_SIZE);
+        writer << length << type;
+    }
 };
 }
