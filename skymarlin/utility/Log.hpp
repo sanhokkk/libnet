94d6bb0cdbde0b69839feedee86eb261bb1acec1
SanitoGonzalez <ddalkisoup23@gmail.com>
2024-02-08 22:07:51 -0800

    Add MIT license to source codes

--- skymarlin/utility/Log.hpp
+++ skymarlin/utility/Log.hpp
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 skymarlin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #pragma once
 
 #include <spdlog/spdlog.h>
@@ -18,39 +42,4 @@ namespace skymarlin::utility
 
 #define SKYMARLIN_LOG_DEBUG(format, ...) \
     spdlog::debug(format __VA_OPT__(,) __VA_ARGS__)
-
-//TODO: `format_string` cannot be passed; it should be a constant expression.
-/*class Log final
-{
-public:
-    template <typename... Args>
-    static void Info(std::string_view format_string, Args... args)
-    {
-        spdlog::info(format_string, args...);
-    }
-
-    template <typename... Args>
-    static void Error(constexpr std::string_view format_string, Args... args)
-    {
-        spdlog::error(format_string, args...);
-    }
-
-    template <typename... Args>
-    static void Warn(constexpr std::string_view format_string, Args... args)
-    {
-        spdlog::warn(format_string, args...);
-    }
-
-    template <typename... Args>
-    static void Critical(constexpr std::string_view format_string, Args... args)
-    {
-        spdlog::critical(format_string, args...);
-    }
-
-    template <typename... Args>
-    static void Debug(constexpr std::string_view format_string, Args... args)
-    {
-        spdlog::debug(format_string, args...);
-    }
-};*/
 }
