fe44e5b612d6bb4c9da1bfe3003e908654624c7f
Sanito Gonzalez <ddalkisoup23@gmail.com>
2024-04-26 19:28:16 +0900

    Add ClientManager::Brodacast

--- skymarlin/net/ClientManager.hpp
+++ skymarlin/net/ClientManager.hpp
@@ -10,6 +10,8 @@ class ClientManager {
 public:
     static void Init(ClientFactory client_factory);
     static std::shared_ptr<Client> CreateClient(boost::asio::io_context& ctx, tcp::socket&& socket);
+    static void Broadcast(std::shared_ptr<flatbuffers::DetachedBuffer> message,
+        std::shared_ptr<Client> except);
 
     inline static util::ConcurrentMap<ClientId, std::shared_ptr<Client>> clients;
 
@@ -26,5 +28,21 @@ inline std::shared_ptr<Client> ClientManager::CreateClient(boost::asio::io_conte
     auto client = client_factory_(ctx, std::move(socket), ++id_generator);
     clients.InsertOrAssign(client->id(), client);
     return client;
+}
+
+inline void ClientManager::Broadcast(std::shared_ptr<flatbuffers::DetachedBuffer> message,
+    std::shared_ptr<Client> except = nullptr) {
+    if (except) {
+        clients.ForEachSome(
+            [&except](const std::shared_ptr<Client>& client) { return client == except; },
+            [](const std::shared_ptr<Client>& client, std::shared_ptr<flatbuffers::DetachedBuffer> message) {
+                client->SendMessage(message);
+            }, std::move(message));
+    } else {
+        clients.ForEachAll([](const std::shared_ptr<Client>& client,
+            std::shared_ptr<flatbuffers::DetachedBuffer> message) {
+                client->SendMessage(message);
+            }, std::move(message));
+    }
 }
 }
