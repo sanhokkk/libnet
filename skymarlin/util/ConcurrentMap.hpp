ba08e21b342a09adffc42e7d03d0914f9c4bb566
Sanito Gonzalez <ddalkisoup23@gmail.com>
2024-04-27 15:13:25 +0900

    Fix ConcurrentMap::ForEachSome no to use std::range

--- skymarlin/util/ConcurrentMap.hpp
+++ skymarlin/util/ConcurrentMap.hpp
@@ -1,37 +1,30 @@
 #pragma once
 
 #include <mutex>
-#include <ranges>
 #include <shared_mutex>
 #include <unordered_map>
 
-namespace skymarlin::util
-{
+namespace skymarlin::util {
 template <typename KeyType, typename ValueType>
-class ConcurrentMap
-{
+class ConcurrentMap {
 public:
     ConcurrentMap() = default;
     ~ConcurrentMap() = default;
-
     ConcurrentMap(const ConcurrentMap&) = delete;
     ConcurrentMap& operator=(const ConcurrentMap&) = delete;
 
-    void InsertOrAssign(const KeyType& key, const ValueType& value)
-    {
-        std::unique_lock lock(mutex);
+    void InsertOrAssign(const KeyType& key, const ValueType& value) {
+        std::unique_lock lock {mutex_};
         map_.insert_or_assign(key, value);
     }
 
-    void InsertOrAssign(KeyType&& key, ValueType&& value)
-    {
-        std::unique_lock lock(mutex);
+    void InsertOrAssign(KeyType&& key, ValueType&& value) {
+        std::unique_lock lock {mutex_};
         map_.insert_or_assign(std::forward<KeyType>(key), std::forward<ValueType>(value));
     }
 
-    bool TryGet(const KeyType& key, ValueType& value) const
-    {
-        std::shared_lock lock(mutex);
+    bool TryGet(const KeyType& key, ValueType& value) const {
+        std::shared_lock lock {mutex_};
 
         auto it = map_.find(key);
         if (it == map_.end()) {
@@ -41,28 +34,24 @@ public:
         return true;
     }
 
-    void Erase(const KeyType& key)
-    {
-        std::unique_lock lock(mutex);
+    void Erase(const KeyType& key) {
+        std::unique_lock lock {mutex_};
         map_.erase(key);
     }
 
-    void Clear()
-    {
-        std::unique_lock lock(mutex);
+    void Clear() {
+        std::unique_lock lock {mutex_};
         map_.clear();
     }
 
-    bool Contains(const KeyType& key) const
-    {
-        std::shared_lock lock(mutex);
+    bool Contains(const KeyType& key) const {
+        std::shared_lock lock {mutex_};
         return map_.contains(key);
     }
 
     template <typename Function, typename... Args> requires std::invocable<Function, ValueType&, Args...>
-    void ForEachAll(Function function, Args... args)
-    {
-        std::shared_lock lock(mutex);
+    void ForEachAll(Function function, Args... args) {
+        std::shared_lock lock {mutex_};
 
         for (auto& [_, value] : map_) {
             function(value, args...);
@@ -73,25 +62,23 @@ public:
         requires std::invocable<Filter, const ValueType&>
         && std::same_as<bool, std::invoke_result_t<Filter, const ValueType&>>
         && std::invocable<Function, ValueType&, Args...>
-    void ForEachSome(Filter filter, Function function, Args... args)
-    {
-        std::shared_lock lock(mutex);
+    void ForEachSome(Filter filter, Function function, Args... args) {
+        std::shared_lock lock {mutex_};
 
-        auto view = map_ | std::views::filter(
-            [&filter](const std::pair<KeyType, ValueType>& pair) { return filter(pair.second); });
-        for (auto& [_, value] : view) {
+        for (auto& [_, value] : map_) {
+            if (!filter(value)) continue;
+
             function(value, args...);
         }
     }
 
-    bool empty() const
-    {
-        std::shared_lock lock(mutex);
+    bool empty() const {
+        std::shared_lock lock {mutex_};
         return map_.empty();
     }
 
 private:
     std::unordered_map<KeyType, ValueType> map_ {};
-    mutable std::shared_mutex mutex {};
+    mutable std::shared_mutex mutex_ {};
 };
 }
